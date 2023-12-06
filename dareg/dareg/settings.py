8f48c23a362c54556ed84396f03d98d745bbf5ef
Adrian Rosinec <adrian@ics.muni.cz>
2023-12-06 01:11:28 +0100

    missing variable

--- dareg/dareg/settings.py
+++ dareg/dareg/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = eval(os.environ["DJANGO_DEBUG"].title())
+PRODUCTION = eval(os.environ["PRODUCTION"].title())
 
 ALLOWED_HOSTS = os.environ["DJANGO_ALLOWED_HOSTS"].split()
 
