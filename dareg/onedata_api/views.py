43fad1b0b0c463f9c83227c32c389b84ea275466
Adrian Rosinec <adrian@ics.muni.cz>
2024-04-09 14:06:15 +0200

    Add new deployment type and file API

--- dareg/onedata_api/views.py
+++ dareg/onedata_api/views.py
@@ -5,9 +5,38 @@ from rest_framework import permissions
 import oneprovider_client
 from onedata_wrapper.api.file_operations_api import FileOperationsApi
 from onedata_wrapper.models.filesystem.dir_entry import DirEntry
+from onedata_wrapper.models.filesystem.file_entry import FileEntry
 from onedata_wrapper.models.space.space_request import SpaceRequest
+from onedata_wrapper.models.filesystem.entry_request import EntryRequest
 from onedata_wrapper.selectors.file_attribute import ALL as FA_ALL
 from api.models import Project, Dataset
+
+class SpacesViewSet(APIView):
+    
+        permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    
+        oneprovider_configuration = oneprovider_client.configuration.Configuration()
+    
+        def get(self, request):
+    
+            if request.GET.get('dataset_id') is None:
+                return Response({"error": "dataset_id is a required parameter"})
+    
+            dataset = Dataset.objects.get(id=request.GET.get('dataset_id'))
+    
+            facility_token = dataset.project.facility.onedata_token
+            if facility_token is None or facility_token == "":
+                return Response({"error": "Dataset doesn't have any supported space. Please contact the administrator of DAREG."})
+    
+            self.oneprovider_configuration.host = dataset.project.facility.onedata_provider_url
+            self.oneprovider_configuration.api_key['X-Auth-Token'] = facility_token
+    
+            file_op_api = FileOperationsApi(self.oneprovider_configuration)
+    
+            # requesting list of spaces from Onedata
+            spaces = file_op_api.get_spaces()
+    
+            return Response({"spaces": spaces})
 
 class FilesViewSet(APIView):
 
@@ -22,36 +51,29 @@ class FilesViewSet(APIView):
 
         dataset = Dataset.objects.get(id=request.GET.get('dataset_id'))
         space_id = ""
-        if request.GET.get('file_id') is None:
-            space_id = dataset.project.onedata_space_id
+        file_id = ""
+        if request.GET.get('file_id') is not None and request.GET.get('file_id') != "" and request.GET.get('file_id') != "null":
+            file_id = request.GET.get('file_id')
         else:
-            space_id = request.GET.get('file_id')
+            file_id = dataset.onedata_file_id
 
         facility_token = dataset.project.facility.onedata_token
-        if space_id is None or facility_token is None or space_id == "" or facility_token == "":
+        if facility_token is None or facility_token == "":
             return Response({"error": "Dataset doesn't have any supported space. Please contact the administrator of DAREG."})
 
         self.oneprovider_configuration.host = dataset.project.facility.onedata_provider_url
         self.oneprovider_configuration.api_key['X-Auth-Token'] = facility_token
 
-        print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", self.oneprovider_configuration.host, self.oneprovider_configuration.api_key['X-Auth-Token'], space_id)
-
         file_op_api = FileOperationsApi(self.oneprovider_configuration)
 
         # creating SpaceRequest object used for Space retrieval (not using API calls yet)
-        space_request = SpaceRequest(space_id=space_id)
+        file_request = EntryRequest(file_id=file_id)
 
         # requesting Space information from Onedata
-        space = file_op_api.get_space(space_request)
+        file = file_op_api.get_file(file_request, FA_ALL)
 
-        # print(space.space_id, space.name)  # space info can be seen here
-        # requesting information about Space root file
-        file_op_api.get_root(space)
-
-        # treating space root directory as usual directory
-        basic_dir = space.root_dir
         # requesting children for actual directory from Onedata
-        file_op_api.get_children(basic_dir, FA_ALL)
+        files = file_op_api.get_children(file, FA_ALL)
 
         
-        return Response({"files": basic_dir})
+        return Response({"files": files})
